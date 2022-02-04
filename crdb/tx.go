0fd01e01f1de7874e449834b3eda70ee61af4f10
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2022-02-04 16:48:00 -0500

    Make it possible to configure maximum retry count when executing transactions.

--- crdb/tx.go
+++ crdb/tx.go
@@ -102,6 +102,26 @@ func Execute(fn func() error) (err error) {
 	}
 }
 
+type txConfigKey struct {}
+
+// WithMaxRetries configures context so that ExecuteTx retries tx specified
+// number of times when encountering retryable errors.
+// Setting retries to 0 will retry indefinitely.
+func WithMaxRetries(ctx context.Context, retries int) context.Context {
+	return context.WithValue(ctx, txConfigKey{}, retries)
+}
+
+const defaultRetries = 50
+
+func numRetriesFromContext(ctx context.Context) int {
+	if v := ctx.Value(txConfigKey{}); v != nil {
+		if retries, ok := v.(int); ok && retries >= 0 {
+			return retries
+		}
+	}
+	return defaultRetries
+}
+
 // ExecuteTx runs fn inside a transaction and retries it as needed. On
 // non-retryable failures, the transaction is aborted and rolled back; on
 // success, the transaction is committed.
